a252f85e1741337c201d43e04aa8779be8d12aa2
Francesco Saverio Galletti <francescosaveriogalletti@gmail.com>
2021-04-22 00:41:09 +0200

    Under construction commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to FraSaGal official Website"/>
+    <div class="absolute bg-indigo-500 w-screen h-screen p-4"></div>
+    <div class="absolute bg-transparent w-screen h-screen p-4">
+        <div
+            class="flex bg-gray-50 h-full w-full overflow-y-scroll text-center items-center p-4"
+        >
+            <div class="flex-grow text-5xl">
+                <p class="text-4xl">
+                    Welcome to <br /><strong>FraSaGal</strong> website.
+                </p>
+                <br /><br />
+                <p class="text-2xl">Currently under construction</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import router from './router.js';
+import App from './App'
+import './assets/styles/index.css';
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+
+// app.use(router);
+
+app.mount('#app')
